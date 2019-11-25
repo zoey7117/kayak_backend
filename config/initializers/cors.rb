d2cff8585fcb3e27bfdf9d2a4ca5b4b7b3e7d2a5
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2019-11-25 13:29:32 -0500

    changed cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,13 +5,23 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+#     origins 'https://kayaks-backend-api.herokuapp.com', 'http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002'
+#
+#     resource '*',
+#       headers: :any,
+#       methods: [:get, :post, :put, :patch, :delete, :options, :head],
+#       credentials: true
+#   end
+# end
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://kayaks-backend-api.herokuapp.com', 'http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002'
-
+    origins '*'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+            credentials: true
+
   end
 end
