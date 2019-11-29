45b63b8f42840daa896b3dbf0281e584a386f012
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2019-11-28 21:33:14 -0500

    remove creditionals

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,13 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://kayaks-backend-api.herokuapp.com', 'http://localhost:3001', 'http://localhost:3002'
+    origins '*'
+    # origins 'https://kayaks-backend-api.herokuapp.com', 'http://localhost:3001', 'http://localhost:3002'
+
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      # credentials: true
   end
 end
