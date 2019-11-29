32bd5ab99e45cfac5ad1cc98abb84d7cc13570e5
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2019-11-28 21:45:17 -0500

    add origins

--- config/initializers/corsV.rb
+++ config/initializers/corsV.rb
@@ -7,11 +7,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*',
+    origins 'https://kayaks-backend-api.herokuapp.com', 'http://localhost:3001', 'http://localhost:3002'
 
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
   end
 end
