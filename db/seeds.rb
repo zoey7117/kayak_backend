c37650c85a28fd1e5dc829036265c7176682c98b
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-02-28 19:41:12 -0500

    postgres

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,6 +19,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+ActiveRecord::Base.connection.tables.each do |table|
+  result = ActiveRecord::Base.connection.execute("SELECT id FROM #{table} ORDER BY id DESC LIMIT 1") rescue ( puts "Warning: not procesing table #{table}. Id is missing?" ; next )
+  ai_val = result.any? ? result.first['id'].to_i + 1 : 1
+  puts "Resetting auto increment ID for #{table} to #{ai_val}"
+  ActiveRecord::Base.connection.execute("ALTER SEQUENCE #{table}_id_seq RESTART WITH #{ai_val}")
+end
+
+
 User.destroy_all
 Trip.destroy_all
 Comment.destroy_all
